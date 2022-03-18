506e9f029f2183e126d53354d49b6b24451a73e8
esnonso <devnellynonso@gmail.com>
2022-03-18 22:27:39 +0100

    Updates

---
+++ precache-manifest.920fb7374677939afc46be4a3f8cfdda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edaaec19f2d88d20e9d10e9cb9b34df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d77c0c2b04f18f74e8f7",
+    "url": "./static/css/main.fff4422d.chunk.css"
+  },
+  {
+    "revision": "6972c8bb073fa26b0daa",
+    "url": "./static/js/2.5ac39dde.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5ac39dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77c0c2b04f18f74e8f7",
+    "url": "./static/js/main.54a7f48b.chunk.js"
+  },
+  {
+    "revision": "5f790dcf136da0545a5f",
+    "url": "./static/js/runtime-main.3f5ac90d.js"
+  }
+]);
